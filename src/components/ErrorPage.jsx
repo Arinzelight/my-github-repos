3bb8cfccb88025c44e09b522682f5578a24e4d5f
Arinzelight <arinzelight2@gmail.com>
2024-05-12 00:26:06 +0100

    add custom 404 page

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,5 +1,29 @@
 import React from "react";
+import { Flex, Heading, Text, Button } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
 export default function ErrorPage() {
-  return <div>404 page not found</div>;
+  return (
+    <Flex
+      direction="column"
+      justify="center"
+      align="center"
+      height="100vh"
+      textAlign="center"
+    >
+      <Heading fontSize="6xl" color="red.500" mb={4}>
+        404
+      </Heading>
+      <Text fontSize="2xl" mb={8}>
+        Oops! Page not found.
+      </Text>
+      <Text fontSize="lg" mb={8}>
+        The page you are looking for might have been removed, had its name
+        changed, or is temporarily unavailable.
+      </Text>
+      <Button colorScheme="blue" as={Link} to="/">
+        Go back to home
+      </Button>
+    </Flex>
+  );
 }
