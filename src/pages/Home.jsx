93783189e4133f212621205542d93d0d38bfff9d
Arinzelight <arinzelight2@gmail.com>
2024-05-12 02:10:04 +0100

    add search functionality and spinner

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,9 @@ import {
   Box,
   Badge,
   Button,
+  Input,
   useBreakpointValue,
+  Spinner,
 } from "@chakra-ui/react";
 import { motion } from "framer-motion";
 
@@ -15,6 +17,8 @@ export default function Home() {
   const [repos, setRepos] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [itemsPerPage] = useState(10);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchRepos = async () => {
@@ -23,23 +27,31 @@ export default function Home() {
           "https://api.github.com/users/Arinzelight/repos"
         );
         setRepos(response.data);
+        setLoading(false);
       } catch (error) {
         console.error("Error fetching repositories:", error);
+        setLoading(false);
       }
     };
 
     fetchRepos();
   }, []);
 
-  const totalPages = Math.ceil(repos.length / itemsPerPage);
-
-  const startIndex = (currentPage - 1) * itemsPerPage;
-  const endIndex = startIndex + itemsPerPage;
-  const currentRepos = repos.slice(startIndex, endIndex);
-
   const handlePageChange = (page) => {
     setCurrentPage(page);
   };
+
+  const filteredRepos = searchQuery
+    ? repos.filter((repo) =>
+        repo.name.toLowerCase().includes(searchQuery.toLowerCase())
+      )
+    : repos;
+
+  const totalPages = Math.ceil(filteredRepos.length / itemsPerPage);
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const currentRepos = filteredRepos.slice(startIndex, endIndex);
 
   // Determine margin based on screen size
   const marginX = useBreakpointValue({ base: 4, lg: 28 });
@@ -49,73 +61,79 @@ export default function Home() {
       <Text fontSize="3xl" fontWeight="bold">
         My GitHub Repositories
       </Text>
-      {currentRepos.length === 0 ? (
-        <Text>Loading...</Text>
-      ) : (
-        currentRepos.map((repo) => (
-          <motion.div
-            key={repo.id}
-            borderWidth="1px"
-            borderRadius="md"
-            boxShadow="md"
-            borderColor="gray.400" // Adjust the border color here
-            p={4}
-            width={{ base: "50%", md: "100%" }}
-            whileHover={{ scale: 1.0199 }}
-            whileTap={{ scale: 0.95 }}
-            initial={{ opacity: 0, y: 20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.5 }}
-            minHeight="120px" // Set fixed height for container
-          >
-            <Box border="1px" borderRadius="md" borderColor="gray.400" p={4}>
-              {" "}
-              {/* Add border styles */}
-              <Link
-                href={repo.html_url}
-                target="_blank"
-                rel="noopener noreferrer"
-                fontWeight="bold"
-                fontSize="xl"
-                color="blue.500"
-              >
-                {repo.name}
-              </Link>
-              <Text mt={2} color="gray.600">
-                {repo.description}
-              </Text>
-              <Box mt={2}>
-                <Badge colorScheme="green" mr={2}>
-                  {repo.language}
-                </Badge>
-                <Text fontSize="sm" color="gray.500">
-                  Last updated: {new Date(repo.updated_at).toLocaleDateString()}
-                </Text>
-              </Box>
-            </Box>
-          </motion.div>
-        ))
+      <Input
+        placeholder="Search repositories..."
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
+      {loading && (
+        <Box w="100%" textAlign="center" color="blue.500">
+          <Text>Loading...</Text>
+          <Spinner size="md" />
+        </Box>
       )}
-      <motion.div
-        className="pagination"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 1 }}
-      >
-        {Array.from({ length: totalPages }).map((_, index) => (
-          <Button
-            key={index}
-            variant={currentPage === index + 1 ? "solid" : "outline"}
-            colorScheme="blue"
-            onClick={() => handlePageChange(index + 1)}
-            whileHover={{ scale: 1.05 }}
-            whileTap={{ scale: 0.95 }}
-            mx={1} // Add margin between buttons
-          >
-            {index + 1}
-          </Button>
-        ))}
-      </motion.div>
+
+      {!loading && (
+        <>
+          {currentRepos.map((repo) => (
+            <motion.div
+              key={repo.id}
+              borderWidth="1px"
+              borderRadius="md"
+              boxShadow="md"
+              borderColor="gray.400" // Adjust the border color here
+              p={4}
+              width={{ base: "50%", md: "100%" }}
+              whileHover={{ scale: 1.0199 }}
+              whileTap={{ scale: 0.95 }}
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.5 }}
+              minHeight="120px" // Set fixed height for container
+            >
+              <Box border="1px" borderRadius="md" borderColor="gray.400" p={4}>
+                <Link
+                  href={repo.html_url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  fontWeight="bold"
+                  fontSize="xl"
+                  color="blue.500"
+                >
+                  {repo.name}
+                </Link>
+                <Text mt={2} color="gray.600">
+                  {repo.description}
+                </Text>
+                <Box mt={2}>
+                  <Badge colorScheme="green" mr={2}>
+                    {repo.language}
+                  </Badge>
+                  <Text fontSize="sm" color="gray.500">
+                    Last updated:{" "}
+                    {new Date(repo.updated_at).toLocaleDateString()}
+                  </Text>
+                </Box>
+              </Box>
+            </motion.div>
+          ))}
+          <Box mt={6}>
+            {Array.from({ length: totalPages }).map((_, index) => (
+              <Button
+                key={index}
+                variant={currentPage === index + 1 ? "solid" : "outline"}
+                colorScheme="blue"
+                onClick={() => handlePageChange(index + 1)}
+                whileHover={{ scale: 1.05 }}
+                whileTap={{ scale: 0.95 }}
+                mx={1} // Add margin between buttons
+              >
+                {index + 1}
+              </Button>
+            ))}
+          </Box>
+        </>
+      )}
     </VStack>
   );
 }
